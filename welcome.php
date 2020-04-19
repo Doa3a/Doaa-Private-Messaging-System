a9c738e864a38f4cdc15f861b8789748f9c8198c
Doa3a <46709700+Doa3a@users.noreply.github.com>
2020-04-19 21:06:37 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -16,6 +16,7 @@ session_start();
 				echo 'Welcome '.htmlentities($_SESSION['user_name'], ENT_QUOTES, 'UTF-8');}
 				else{
 					header("location:index.php");}?>
+                             <a style ="float:right" href="Logout.php" >Logout</a>
 		</div>
 <div class = "section2"> 
       <a style="color:blue;">Send a new message:</a>
